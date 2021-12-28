b293c89a23840fd5fe6a1bb26c8ba73a7ede4f76
rdhurlbu <rdhurlbu@ncsu.edu>
2021-12-28 12:53:26 -0500

    Initial project structure

---
+++ ReplicaWrapper.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 28 12:49:35 2021
+
+@author: Ryan Hurlbut
+"""
+
---
+++ ResembleWrapper.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 28 12:49:46 2021
+
+@author: Ryan Hurlbut
+"""
+
