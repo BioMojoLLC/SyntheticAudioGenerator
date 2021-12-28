1e0338b5e2fa0df67e52bf2d57e681977bc77393
rdhurlbu <rdhurlbu@ncsu.edu>
2021-12-28 15:15:48 -0500

    [Code] Main program execution. Uses queues to loop through terms, sentences, and APIs. Also made a load_data module

---
+++ load_data.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 28 13:39:27 2021
+
+@author: Ryan Hurlbut
+"""
+
+import os
+
+def prep_text(text_dir, keywords:[]) -> dict:
+    """Loads text data from the text directory, and only keeps sentences associated
+        with a key word. 
+        
+        RETURNS
+            A dictionary with keywords as the key, and a list of sentences as the value
+    """
+    sentences = {kw:[] for kw in keywords}
+    text_files = os.listdir(text_dir)
+    for filename in text_files:
+        print('Reading ---',  filename)
+        contents = []
+        with open(text_dir + filename, 'r') as file:
+            contents = [line.strip() for line in file.readlines()]
+        
+        for line in contents:
+            for kw in keywords:
+                if ' ' + kw + ' ' in line:
+                    sentences[kw].append(line)
+                    break # no need to continue, we found a matching line of text
+    
--- main.py
+++ main.py
@@ -5,3 +5,83 @@ Created on Tue Dec 28 12:48:38 2021
 @author: Ryan Hurlbut
 """
 
+""" These will end up being CL options"""
+from settings import audio_dir
+from settings import text_dir
+from settings import keyword_file
+from settings import out_file
+from load_data import prep_text
+
+
+import os
+import numpy as np
+from collections import deque
+import random
+import csv
+
+import ResembleWrapper
+import ReplicaWrapper
+
+def bytes_to_mins(byte_count : int) -> float:
+    # TODO
+    pass
+
+if __name__ == '__main__':
+    sentences_per_term = 5
+    minutes_per_term = 10
+    
+    with open(keyword_file, 'r') as file:
+        keywords = file.read().splitlines()
+    
+    # Load and sort text data into a dictionary {keyword:[text1,text2,. . .]}
+    sentences = prep_text(text_dir, keywords)
+    
+    # Print stats, pick a random selection from each list
+    for k, v in sentences.items():
+        print("Sentences found for", k, ":", len(v))
+        sentences[k] = np.random.choice( v, sentences_per_term , replace=False)
+    
+    print("Saving up to ", sentences_per_term, "sentences per term")
+    
+    # APIS that you wish to use. We may add these as command line options. 
+    # For now they are hardcoded in. 
+    apis = []
+    apis.append(ResembleWrapper())
+    apis.append(ReplicaWrapper())
+    
+    # Convert the lists to queues, we want to generate on each term using a queue of sentences.
+    for v in sentences.values():
+        v = deque(v)
+
+    # Rotate through terms and API's, until we have made enough for each term or run out of service resources
+    out_data = []
+    term_q = deque(keywords)
+    term_bytes = {kw:0 for kw in keywords}
+    api_q = deque(apis)
+    while len(term_q) and len(api_q):
+        term = term_q.pop_left()
+        api = api_q.pop_left()
+        sentence = sentences[term].pop_left()
+        sentences[term].append(sentence)
+        
+        resp, filename, size = api.generate_audio(text_dir, sentence) 
+        if resp == 200:
+            out_data.append([filename, size, sentence])
+            term_bytes[term] += size
+            api_q.append(api) # only add api back if we had a success
+        else:
+            pass # May do something later
+            
+        if bytes_to_mins(term_bytes[term]) < minutes_per_term:
+            term_q.append(term)
+        
+        # Randomly offset the terms, this will ensure each term does not get used by the same api repeatedly. 
+        if random.getrandbits(1):
+            term_q.append(term_q.pop_left)
+    
+    for t, b in term_bytes:
+        print(b, "Bytes made for term:", t)
+        
+    # with open(audio_dir + out_file, 'w', newline='' ) as file:
+    #     writer = csv.writer(file, delimeter=',')
+    #     writer.writerows(out_data)
