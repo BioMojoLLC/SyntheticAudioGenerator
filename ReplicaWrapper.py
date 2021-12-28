660f2f962771c35e1955eff7e567e34de8c5425b
jrbream <jrbream@ncsu.edu>
2021-12-28 13:53:46 -0500

    ServiceWrapper implemented, Replica auth implemented

--- ReplicaWrapper.py
+++ ReplicaWrapper.py
@@ -5,3 +5,25 @@ Created on Tue Dec 28 12:49:35 2021
 @author: Ryan Hurlbut
 """
 
+import requests
+
+from SyntheticAudioGenerator.ServiceWrapper import ServiceWrapperInterface
+
+
+class ReplicaWrapper(ServiceWrapperInterface):
+
+    def authenticate(self):
+        print("Enter Replica account information")
+        user_id = input("username: ")
+        password = input("password: ")
+
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        payload = 'client_id=' + user_id + '&secret=' + password
+
+        # send auth request
+        r = requests.post('https://api.replicastudios.com/auth', headers = headers, data = payload)
+        auth_token = r.json()['access_token']
+        refresh_token = r.json()['refresh_token']
+
+        print("Replica auth_token: " + auth_token)
+        print("Replica refresh_token: " + refresh_token)
--- ServiceWrapper.py
+++ ServiceWrapper.py
@@ -3,5 +3,21 @@
 Created on Tue Dec 28 12:49:10 2021
 
 @author: Ryan Hurlbut
+@author: Jacob Bream
 """
 
+class ServiceWrapperInterface:
+    api_token = None
+
+    def __init__(self, voices):
+        self.voices = voices
+        self.authenticate()
+
+    def authenticate(self):
+        pass
+
+    def generateAudio(self, output_folder, sentence):
+        pass
+
+    def saveAudio(self):
+        pass
